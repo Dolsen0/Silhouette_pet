9bc7d962e57a6ad0eb23e4ad9c906962a07797b1
dolsen0 <derek.olsen@gmail.com>
2023-01-30 22:43:43 -0500

    added user input into app

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from secretPass import apiPass
 
 openai.api_key = apiPass
 
-#pet = input('what kind of pet do you want to see?')
-#personality = input('what is their personality like?')
-#location = input('where would you like them to be?')
+pet = input('what kind of pet do you want to see?')
+personality = input('what is their personality like?')
+location = input('where would you like them to be?')
 
 
 response = openai.Image.create(
-    prompt=f"a silhouette of a cat near a boat",
+    prompt=f"a hand drawn style silhouette of a {personality} {pet} near a {location}",
     n=1,
     size = "512x512"
 )
